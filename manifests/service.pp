ecf800df6e36d8f47f5e992ec7692e12dc684797
Roman A. Chukov <r.chukov@asgardahost.ru>
2021-03-11 18:40:42 +0000

    Fixed a typo in service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,13 +1,13 @@
 class k8s_workernode::service ( Boolean $k8s_kube_proxy_service_enable, Boolean $k8s_kubelet_service_enable  ) {
     service { "k8s-kubeproxy" :
-        ensure => $k8s_api_service_enable,
-        enable => $k8s_api_service_enable,
+        ensure => $k8s_kube_proxy_service_enable,
+        enable => $k8s_kube_proxy_service_enable,
         require => Class[ "k8s_workernode::install" ],
     }
 
     service { "k8s-kubelet" :
-        ensure => $k8s_control_manager_service_enable,
-        enable => $k8s_control_manager_service_enable,
+        ensure => $k8s_kubelet_service_enable,
+        enable => $k8s_kubelet_service_enable,
         require => Class[ "k8s_workernode::install" ],
     }
 
